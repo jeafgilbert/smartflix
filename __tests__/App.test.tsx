8dfccf43b1dea92e0d63e401886148af5bcbb079
Jeaf Gilbert <jeafgilbert@gmail.com>
2020-08-29 15:05:14 +0700

    Updated Project Structure

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
 import 'react-native'
 import * as renderer from 'react-test-renderer'
-import { App } from './app'
+import { App } from '../App'
 
 test('renders correctly', () => {
-  const tree = renderer.create(
-    <App />,
-  )
+  const tree = renderer.create(<App />);
   expect(tree).toBeDefined()
 })
